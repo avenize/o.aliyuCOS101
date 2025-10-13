6a236eb6bee7a76d0f0d03f083439ae62a95879e
Victoria Aliyu <avenizee@gmail.com>
2025-10-13 16:11:12 +0100

    (my week-3 practice commit)

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    //addition
+    let sum = 5550 + 7310;
+    println!("The sum of 5555 and 7310 = {}", sum);
+
+    //substraction
+    let difference:f32 = 95.5 - 4.3;
+    println!("The differenceof 95.5 and 4.3 = {}", difference);
+
+    //multiplication
+    let product:f32 = 4.0 * 30.0;
+    println!("THe multiple of 4 and 30 = {}", product);
+
+    //division
+    let quotient:f32 = 56.7 / 32.2;
+    println!("The division of 56.7 and 32.2s = {}", quotient);
+
+    //remainder
+    let remainder:u32 = 43 % 5;
+    println!("The remainder of 43 and 5 = {}", remainder);
+}
---
+++ week-3/practice_3/target/debug/practice_3.d
@@ -0,0 +1,1 @@
+C:\Users\Oyiza\OneDrive\Dokumente\o.aliyuCOS101\week-3\practice_3\target\debug\practice_3.exe: C:\Users\Oyiza\OneDrive\Dokumente\o.aliyuCOS101\week-3\practice_3\src\main.rs
