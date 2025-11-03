c1150ee4f70bf9f2513a0d1e0beecd46832b3c92
Victoria Aliyu <avenizee@gmail.com>
2025-11-03 23:57:18 +0100

    (practice 6)

--- week-6/practice_6/src/main.rs
+++ week-6/practice_6/src/main.rs
@@ -5,6 +5,14 @@ fn main() {
     let n4 = n1 + &n2 + &n3;  // n2 & n3 reference is passed
 
 
-    //About Electrical
+    //About Electrical/Electronic
+    println!("\nThe {} is informed by the aspiration to train electrical?electronic engineeering professionals in the ares of design, building and maintenance electrical control systems", n4);
+
+    let w1 = "Computer".to_string();
+    let w2 = "Computer".to_string();
+    let w3 = w1 + &w2; // w2 reference is passed
+    println!();
+    println!("{} is aimed at developing competent, creative, innovative, entreppreneurial and ethically minded persons, capable of creating value in the diverse fields of Computer science.", w3);
+
 
 }
