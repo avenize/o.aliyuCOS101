2a633b2fcc4445ac95244ab23e9a1ede1a9fe479
Victoria Aliyu <avenizee@gmail.com>
2025-11-17 23:41:21 +0100

    week-8

---
+++ week-8/practice-11/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    //array of numbers
+    let numbers = [1,2,3,4,];
+    println!("Original array={:?}", numbers);
+
+    //creta a slice of 2nd and 3rd element
+    let slice1 = &numbers[1..3];
+    println!("2nd and 3rd elements sliced ={:?}", slice1);
+
+    //omit the start index
+    let slice2 
+}
